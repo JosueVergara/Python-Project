7621958a0dcc35342d7e172111cf01186f35f24f
Josue <1337x.to.io@gmail.com>
2023-02-21 19:16:21 -0500

    Fixed some bugs

--- data_processing/charts.py
+++ data_processing/charts.py
@@ -16,5 +16,4 @@ def generate_pie_chart(labels, values):
 if __name__ == '__main__':
   labels = ['a', 'b', 'c']
   values = [10, 40, 800]
-  # generate_bar_chart(labels, values)
   generate_pie_chart(labels, values)

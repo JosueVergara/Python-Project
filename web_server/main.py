15ee913aad5f5b6c48118bb73f538ebd3b768dd2
Josue <1337x.to.io@gmail.com>
2023-02-23 20:02:46 -0500

    Server  built using fastApi

--- web_server/main.py
+++ web_server/main.py
@@ -1,4 +1,19 @@
 import store
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def get_list():
+    return [5,2,645,23,12]
+
+
+@app.get("/contact")
+def get_list():
+    return {"name": "Joshua",
+            "lastName": "Jones",
+            "age": 25}
+
 
 def run():
     store.categories()
